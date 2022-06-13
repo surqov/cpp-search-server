786099afdda02a94c238002e639245f20773bb4e
root <root@DESKTOP-B4HK3C3.localdomain>
2022-06-14 01:05:35 +0300

    find the right answer & solution

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,24 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь: 190
-
+// Напишите ответ здесь: 271 
+//
 // Закомитьте изменения и отправьте их в свой репозиторий.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+    int the_number = 3;
+    for (int i = 0; i < 1000; ++i) {
+        if ((i < 10) 
+            && (i / the_number == 1) && (i % the_number == 0)) {
+            count += 1;   
+        } else if ((i >= 10) && (i < 100) 
+                    && ((i / 10 == the_number) || (i % 10 == the_number))) {
+            count += 1;
+        } else if ((i >= 100) 
+                    && ((i / 100 == the_number) || (i % 100 / 10 == the_number) || (i % 10 == the_number))) {
+            count += 1;     
+        }
+    }
+    cout << count << endl;
+}
