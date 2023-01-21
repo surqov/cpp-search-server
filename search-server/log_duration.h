19b954263e56b1be1c3006d07d6b058977c90366
Gleb Surkov <writeln7@gmail.com>
2023-01-21 13:37:45 +0100

    added log_duration again :)

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -1,8 +1,32 @@
-Ошибка компиляции:
-/exposed/submission/main.cpp: In lambda function:
-/exposed/submission/main.cpp:60:31: error: could not convert ‘(& key_words)->std::set<std::__cxx11::basic_string<char>, std::less<void> >::find((* & w))’ from ‘std::set<std::__cxx11::basic_string<char>, std::less<void> >::const_iterator’ {aka ‘std::_Rb_tree<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char>, std::_Identity<std::__cxx11::basic_string<char> >, std::less<void>, std::allocator<std::__cxx11::basic_string<char> > >::const_iterator’} to ‘bool’
-   60 |             if (key_words.find(w)) {
-      |                 ~~~~~~~~~~~~~~^~~
-      |                               |
-      |                               std::set<std::__cxx11::basic_string<char>, std::less<void> >::const_iterator {aka std::_Rb_tree<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char>, std::_Identity<std::__cxx11::basic_string<char> >, std::less<void>, std::allocator<std::__cxx11::basic_string<char> > >::const_iterator}
-compilation terminated due to -Wfatal-errors.
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+
+class LogDuration {
+public:
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string& id) : id_(id) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+};
